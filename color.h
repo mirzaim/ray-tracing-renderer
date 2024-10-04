1c07b72884c6f3beb26b817b4f62d4a1539829d5
Morteza Mirzaei <mortezamirzai120@gmail.com>
2024-10-04 10:47:21 -0400

    Add Gamma Correction.

--- color.h
+++ color.h
@@ -5,10 +5,14 @@
 
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+  return (linear_component > 0) ? std::sqrt(linear_component) : 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
-  int ir = static_cast<int>(std::round(255 * pixel_color.x()));
-  int ig = static_cast<int>(std::round(255 * pixel_color.y()));
-  int ib = static_cast<int>(std::round(255 * pixel_color.z()));
+  int ir = static_cast<int>(std::round(255 * linear_to_gamma(pixel_color.x())));
+  int ig = static_cast<int>(std::round(255 * linear_to_gamma(pixel_color.y())));
+  int ib = static_cast<int>(std::round(255 * linear_to_gamma(pixel_color.z())));
 
   out << ir << ' ' << ig << ' ' << ib << '\n';
 }
